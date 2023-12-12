615daa9452533a8a271aa65110e6f283069da521
Killian Habasque <khabasque@doowup.fr>
2023-12-12 18:55:14 +0100

    feat generation dice

---
+++ README.md
@@ -0,0 +1,42 @@
+# Dice roller with Three.js and cannon-es
+
+Demo for the tutorial on how to code a dice cube and realistic dice roller
+
+![Box preview](./preview/dice-preview.gif)
+
+[Article on Codrops](https://tympanus.net/codrops/?p=69521)
+
+[Demo](http://tympanus.net/Tutorials/DiceRoller/)
+
+## Installation
+
+No package manager / build system is needed.
+You can run the page as it is on local server (any web server, really).
+
+The page is using the following libs:
+
+1) Three.js + their addons OrbitControls and mergeBufferGeometries.
+   https://threejs.org/docs/#manual/en/introduction/Installation
+
+2) cannon-es
+   https://github.com/pmndrs/cannon-es
+
+Both tools are added as JS modules via CDN with import map. Other installation options are available (see the links above)
+
+## Misc
+
+Follow Ksenia: [Twitter](https://twitter.com/uuuuuulala), [Codepen](https://codepen.io/ksenia-k), [website](https://ksenia-k.com/)
+
+Follow Codrops: [Twitter](http://www.twitter.com/codrops), [Facebook](http://www.facebook.com/codrops)
+, [GitHub](https://github.com/codrops), [Instagram](https://www.instagram.com/codropsss/)
+
+## License
+
+[MIT](LICENSE)
+
+Made with :blue_heart:  by [Codrops](http://www.codrops.com)
+
+
+
+
+
---
+++ js/main.js
@@ -0,0 +1,389 @@
+import * as CANNON from 'https://cdn.skypack.dev/cannon-es';
+
+import * as THREE from 'three';
+import * as BufferGeometryUtils from 'three/addons/utils/BufferGeometryUtils.js';
+import * as dat from 'dat.gui';
+
+const gui = new dat.GUI();
+
+const canvasEl = document.querySelector('#canvas');
+const scoreResult = document.querySelector('#score-result');
+const rollBtn = document.querySelector('#roll-btn');
+
+let renderer, scene, camera, diceMesh, physicsWorld;
+
+const params = {
+    numberOfDice: 2,
+    segments: 40,
+    edgeRadius: .07,
+    notchRadius: .12,
+    notchDepth: .1,
+};
+
+const diceArray = [];
+
+initPhysics();
+initScene();
+
+window.addEventListener('resize', updateSceneSize);
+// window.addEventListener('dblclick', throwDice);
+rollBtn.addEventListener('click', throwDice);
+
+function initScene() {
+
+    renderer = new THREE.WebGLRenderer({
+        alpha: true,
+        antialias: true,
+        canvas: canvasEl
+    });
+    renderer.shadowMap.enabled = true
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+    scene = new THREE.Scene();
+
+    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, .1, 300)
+    camera.position.set(0, 15, 15);
+    camera.rotation.set(18, 0, 0);
+
+    updateSceneSize();
+
+    const ambientLight = new THREE.AmbientLight(0xffffff, .5);
+    scene.add(ambientLight);
+    const topLight = new THREE.PointLight(0xffffff, .5);
+    topLight.position.set(10, 15, 0);
+    topLight.castShadow = true;
+    topLight.shadow.mapSize.width = 2048;
+    topLight.shadow.mapSize.height = 2048;
+    topLight.shadow.camera.near = 5;
+    topLight.shadow.camera.far = 400;
+    scene.add(topLight);
+
+    createFloor();
+    diceMesh = createDiceMesh();
+    for (let i = 0; i < params.numberOfDice; i++) {
+        diceArray.push(createDice());
+        addDiceEvents(diceArray[i]);
+    }
+
+    let scaleToggle = false;
+
+    throwDice();
+
+    // Mon code 
+    const perspectiveCameraFolder = gui.addFolder('Camera');
+    perspectiveCameraFolder.add(camera.position, 'x', 0, 50);
+    perspectiveCameraFolder.add(camera.position, 'y', 0, 50);
+    perspectiveCameraFolder.add(camera.position, 'z', 0, 50);
+    perspectiveCameraFolder.add(camera.rotation, 'x', 0, 90, 1);
+    perspectiveCameraFolder.add(camera.rotation, 'y', 0, 50);
+    perspectiveCameraFolder.add(camera.rotation, 'z', 0, 50);
+    perspectiveCameraFolder.open()
+
+    window.addEventListener('click', onDocumentMouseDown, false);
+
+
+    var raycaster = new THREE.Raycaster();
+    var mouse = new THREE.Vector2();
+    function onDocumentMouseDown(event) {
+        event.preventDefault();
+        mouse.x = (event.clientX / renderer.domElement.clientWidth) * 2 - 1;
+        mouse.y = - (event.clientY / renderer.domElement.clientHeight) * 2 + 1;
+        raycaster.setFromCamera(mouse, camera);
+        console.log(scene.children);
+        var intersects = raycaster.intersectObjects(scene.children,);
+        console.log(intersects);
+        if (intersects.length > 3) {
+            const selectedObject = intersects[0].object;
+    
+            if (selectedObject.parent && selectedObject.parent.children.length > 0) {
+                const parentObject = selectedObject.parent;
+    
+                for (let i = 0; i < parentObject.children.length; i++) {
+                    const child = parentObject.children[i];
+    
+                    if (scaleToggle) {
+                        child.position.set(0, 0, 0);
+                    } else {
+                        child.position.set(1.2, 1.2, 1.2);
+                    }
+                }
+    
+                scaleToggle = !scaleToggle;
+            }
+        }
+    }
+
+    render();
+}
+
+function initPhysics() {
+    physicsWorld = new CANNON.World({
+        allowSleep: true,
+        gravity: new CANNON.Vec3(0, -50, 0),
+    })
+    physicsWorld.defaultContactMaterial.restitution = .3;
+}
+
+function createFloor() {
+    const floor = new THREE.Mesh(
+        new THREE.PlaneGeometry(1000, 1000),
+        new THREE.ShadowMaterial({
+            opacity: .1
+        })
+    )
+    floor.receiveShadow = true;
+    floor.position.y = -7;
+    floor.quaternion.setFromAxisAngle(new THREE.Vector3(-1, 0, 0), Math.PI * .5);
+    scene.add(floor);
+
+    const floorBody = new CANNON.Body({
+        type: CANNON.Body.STATIC,
+        shape: new CANNON.Plane(),
+    });
+    floorBody.position.copy(floor.position);
+    floorBody.quaternion.copy(floor.quaternion);
+    physicsWorld.addBody(floorBody);
+}
+
+function createDiceMesh() {
+    const boxMaterialOuter = new THREE.MeshStandardMaterial({
+        color: 0xeeeeee,
+    })
+    const boxMaterialInner = new THREE.MeshStandardMaterial({
+        color: 0x000000,
+        roughness: 0,
+        metalness: 1,
+        side: THREE.DoubleSide
+    })
+
+    const diceMesh = new THREE.Group();
+    const innerMesh = new THREE.Mesh(createInnerGeometry(), boxMaterialInner);
+    let outerMesh = new THREE.Mesh(createBoxGeometry(), boxMaterialOuter);
+    // diceMesh.callback = function() { console.log("test");}
+    outerMesh.castShadow = true;
+    diceMesh.add(innerMesh, outerMesh);
+    // diceMesh.callback = function() { console.log("test"); }
+
+
+    return diceMesh;
+}
+
+function createDice() {
+    const mesh = diceMesh.clone();
+    scene.add(mesh);
+
+    const body = new CANNON.Body({
+        mass: 1,
+        shape: new CANNON.Box(new CANNON.Vec3(.5, .5, .5)),
+        sleepTimeLimit: .1
+    });
+    physicsWorld.addBody(body);
+
+    return { mesh, body };
+}
+function createBoxGeometry() {
+
+    let boxGeometry = new THREE.BoxGeometry(1, 1, 1, params.segments, params.segments, params.segments);
+    // boxGeometry.callback = function() { console.log("dsfdsfds"); }
+    const positionAttr = boxGeometry.attributes.position;
+    const subCubeHalfSize = .5 - params.edgeRadius;
+
+
+    for (let i = 0; i < positionAttr.count; i++) {
+
+        let position = new THREE.Vector3().fromBufferAttribute(positionAttr, i);
+
+        const subCube = new THREE.Vector3(Math.sign(position.x), Math.sign(position.y), Math.sign(position.z)).multiplyScalar(subCubeHalfSize);
+        const addition = new THREE.Vector3().subVectors(position, subCube);
+
+        if (Math.abs(position.x) > subCubeHalfSize && Math.abs(position.y) > subCubeHalfSize && Math.abs(position.z) > subCubeHalfSize) {
+            addition.normalize().multiplyScalar(params.edgeRadius);
+            position = subCube.add(addition);
+        } else if (Math.abs(position.x) > subCubeHalfSize && Math.abs(position.y) > subCubeHalfSize) {
+            addition.z = 0;
+            addition.normalize().multiplyScalar(params.edgeRadius);
+            position.x = subCube.x + addition.x;
+            position.y = subCube.y + addition.y;
+        } else if (Math.abs(position.x) > subCubeHalfSize && Math.abs(position.z) > subCubeHalfSize) {
+            addition.y = 0;
+            addition.normalize().multiplyScalar(params.edgeRadius);
+            position.x = subCube.x + addition.x;
+            position.z = subCube.z + addition.z;
+        } else if (Math.abs(position.y) > subCubeHalfSize && Math.abs(position.z) > subCubeHalfSize) {
+            addition.x = 0;
+            addition.normalize().multiplyScalar(params.edgeRadius);
+            position.y = subCube.y + addition.y;
+            position.z = subCube.z + addition.z;
+        }
+
+        const notchWave = (v) => {
+            v = (1 / params.notchRadius) * v;
+            v = Math.PI * Math.max(-1, Math.min(1, v));
+            return params.notchDepth * (Math.cos(v) + 1.);
+        }
+        const notch = (pos) => notchWave(pos[0]) * notchWave(pos[1]);
+
+        const offset = .23;
+
+        if (position.y === .5) {
+            position.y -= notch([position.x, position.z]);
+        } else if (position.x === .5) {
+            position.x -= notch([position.y + offset, position.z + offset]);
+            position.x -= notch([position.y - offset, position.z - offset]);
+        } else if (position.z === .5) {
+            position.z -= notch([position.x - offset, position.y + offset]);
+            position.z -= notch([position.x, position.y]);
+            position.z -= notch([position.x + offset, position.y - offset]);
+        } else if (position.z === -.5) {
+            position.z += notch([position.x + offset, position.y + offset]);
+            position.z += notch([position.x + offset, position.y - offset]);
+            position.z += notch([position.x - offset, position.y + offset]);
+            position.z += notch([position.x - offset, position.y - offset]);
+        } else if (position.x === -.5) {
+            position.x += notch([position.y + offset, position.z + offset]);
+            position.x += notch([position.y + offset, position.z - offset]);
+            position.x += notch([position.y, position.z]);
+            position.x += notch([position.y - offset, position.z + offset]);
+            position.x += notch([position.y - offset, position.z - offset]);
+        } else if (position.y === -.5) {
+            position.y += notch([position.x + offset, position.z + offset]);
+            position.y += notch([position.x + offset, position.z]);
+            position.y += notch([position.x + offset, position.z - offset]);
+            position.y += notch([position.x - offset, position.z + offset]);
+            position.y += notch([position.x - offset, position.z]);
+            position.y += notch([position.x - offset, position.z - offset]);
+        }
+
+        positionAttr.setXYZ(i, position.x, position.y, position.z);
+    }
+
+
+    boxGeometry.deleteAttribute('normal');
+    boxGeometry.deleteAttribute('uv');
+    boxGeometry = BufferGeometryUtils.mergeVertices(boxGeometry);
+
+    boxGeometry.computeVertexNormals();
+
+
+    // Mon code
+    // renderer.domElement.addEventListener("click", onDblClick);
+    // function onDblClick(event){
+    //     alert('hi');
+    // }
+    return boxGeometry;
+}
+
+function createInnerGeometry() {
+    const baseGeometry = new THREE.PlaneGeometry(1 - 2 * params.edgeRadius, 1 - 2 * params.edgeRadius);
+    const offset = .48;
+    return BufferGeometryUtils.mergeBufferGeometries([
+        baseGeometry.clone().translate(0, 0, offset),
+        baseGeometry.clone().translate(0, 0, -offset),
+        baseGeometry.clone().rotateX(.5 * Math.PI).translate(0, -offset, 0),
+        baseGeometry.clone().rotateX(.5 * Math.PI).translate(0, offset, 0),
+        baseGeometry.clone().rotateY(.5 * Math.PI).translate(-offset, 0, 0),
+        baseGeometry.clone().rotateY(.5 * Math.PI).translate(offset, 0, 0),
+    ], false);
+}
+
+function addDiceEvents(dice) {
+    dice.body.addEventListener('sleep', (e) => {
+
+        dice.body.allowSleep = false;
+
+        const euler = new CANNON.Vec3();
+        e.target.quaternion.toEuler(euler);
+
+        const eps = .1;
+        let isZero = (angle) => Math.abs(angle) < eps;
+        let isHalfPi = (angle) => Math.abs(angle - .5 * Math.PI) < eps;
+        let isMinusHalfPi = (angle) => Math.abs(.5 * Math.PI + angle) < eps;
+        let isPiOrMinusPi = (angle) => (Math.abs(Math.PI - angle) < eps || Math.abs(Math.PI + angle) < eps);
+
+
+        if (isZero(euler.z)) {
+
+            if (isZero(euler.x)) {
+                showRollResults(1);
+                dice.mesh.callback = function () { console.log(1); }
+
+            } else if (isHalfPi(euler.x)) {
+                showRollResults(4);
+                dice.mesh.callback = function () { console.log(4); }
+            } else if (isMinusHalfPi(euler.x)) {
+                showRollResults(3);
+                dice.mesh.callback = function () { console.log(3); }
+            } else if (isPiOrMinusPi(euler.x)) {
+                showRollResults(6);
+                dice.mesh.callback = function () { console.log(6); }
+            } else {
+                // landed on edge => wait to fall on side and fire the event again
+                dice.body.allowSleep = true;
+            }
+        } else if (isHalfPi(euler.z)) {
+            showRollResults(2);
+            dice.mesh.callback = function () { console.log(2); }
+        } else if (isMinusHalfPi(euler.z)) {
+            showRollResults(5);
+            dice.mesh.callback = function () { console.log(5); }
+        } else {
+            // landed on edge => wait to fall on side and fire the event again
+            dice.body.allowSleep = true;
+        }
+    });
+    // dice.body.callback = function() { console.log("test"); }
+    // dice.mesh.callback = function() { console.log("dsfsdfds"); }
+}
+
+function showRollResults(score) {
+    if (scoreResult.innerHTML === '') {
+        scoreResult.innerHTML += score;
+    } else {
+        scoreResult.innerHTML += ('+' + score);
+    }
+}
+
+function render() {
+    physicsWorld.fixedStep();
+
+    for (const dice of diceArray) {
+        dice.mesh.position.copy(dice.body.position)
+        dice.mesh.quaternion.copy(dice.body.quaternion)
+        // dice.callback = function() { console.log("test"); }
+        // console.log(dice.body.quaternion)
+        // dice.mesh.callback = function() { console.log("test"); }
+    }
+
+    renderer.render(scene, camera);
+    requestAnimationFrame(render);
+}
+
+function updateSceneSize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+function throwDice() {
+    scoreResult.innerHTML = '';
+
+    diceArray.forEach((d, dIdx) => {
+
+        d.body.velocity.setZero();
+        d.body.angularVelocity.setZero();
+
+        d.body.position = new CANNON.Vec3(6, dIdx * 1.5, 0);
+        d.mesh.position.copy(d.body.position);
+
+        d.mesh.rotation.set(2 * Math.PI * Math.random(), 0, 2 * Math.PI * Math.random())
+        d.body.quaternion.copy(d.mesh.quaternion);
+
+        const force = 3 + 5 * Math.random();
+        d.body.applyImpulse(
+            new CANNON.Vec3(-force, force, 0),
+            new CANNON.Vec3(0, 0, .2)
+        );
+        // d.mesh.callback = function() { console.log("test"); }
+        d.body.allowSleep = true;
+    });
+}
